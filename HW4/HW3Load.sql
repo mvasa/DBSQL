15b63a37daa3edf5c4e7dd48e63d4bb43594a06a
mvasa <31524613+mvasa@users.noreply.github.com>
2017-10-21 12:24:05 -0600

    Add files via upload

---
+++ HW4/HW3Load.sql
@@ -0,0 +1,12 @@
+\COPY A.Airport FROM 'C:\Users\manoj\Desktop\DBHW_local_copy\DBSQL\airlines_HW\Airport.csv' DELIMITER ',' CSV;
+
+\COPY A.Aircraft FROM 'C:\Users\manoj\Desktop\DBHW_local_copy\DBSQL\airlines_HW\Aircraft.csv' DELIMITER ',' CSV;
+
+\COPY A.Carrier FROM 'C:\Users\manoj\Desktop\DBHW_local_copy\DBSQL\airlines_HW\Carrier.csv' DELIMITER ',' CSV;
+
+\COPY A.City FROM 'C:\Users\manoj\Desktop\DBHW_local_copy\DBSQL\airlines_HW\City.csv' DELIMITER ',' CSV;
+
+\COPY A.state FROM 'C:\Users\manoj\Desktop\DBHW_local_copy\DBSQL\airlines_HW\state.csv' DELIMITER ',' CSV;
+
+\COPY A.Flight_Summary FROM 'C:\Users\manoj\Desktop\DBHW_local_copy\DBSQL\airlines_HW\Flight_Summary.csv' DELIMITER ',' CSV;
+
