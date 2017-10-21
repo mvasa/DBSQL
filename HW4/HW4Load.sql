6baca0afd6c45836839bef7137aeb7980eac3764
mvasa <31524613+mvasa@users.noreply.github.com>
2017-10-21 12:35:42 -0600

    changed : hw3... to hw4...

---
+++ HW4/HW4Load.sql
@@ -0,0 +1,12 @@
+\COPY A.Airport FROM 'C:\Users\manoj\Desktop\DBSQL\HW4\Airport.csv' DELIMITER ',' CSV;
+
+\COPY A.Aircraft FROM 'C:\Users\manoj\Desktop\DBSQL\HW4\Aircraft.csv' DELIMITER ',' CSV;
+
+\COPY A.Carrier FROM 'C:\Users\manoj\Desktop\DBSQL\HW4\Carrier.csv' DELIMITER ',' CSV;
+
+\COPY A.City FROM 'C:\Users\manoj\Desktop\DBSQL\HW4\City.csv' DELIMITER ',' CSV;
+
+\COPY A.state FROM 'C:\Users\manoj\Desktop\DBSQL\HW4\state.csv' DELIMITER ',' CSV;
+
+\COPY A.Flight_Summary FROM 'C:\Users\manoj\Desktop\DBSQL\HW4\Flight_Summary.csv' DELIMITER ',' CSV;
+
